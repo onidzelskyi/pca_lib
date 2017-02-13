12bc389b2c961a263b3f6601b976d6c6de9a25ec
onidzelskyi <alexey.education@gmail.com>
2017-02-13 14:39:55 +0200

    Updated repo

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -17,7 +17,7 @@ class TestPCA(TestCase):
         self.x = np.array(array)
 
     def test_pca_methods_are_equal(self):
-        """Check if reduced matrices are equal to 5 digit after point for two PCA methods."""
+        """Check if reduced matrices are equal to 4 digit after point for two PCA methods."""
         x_reduced_svd, _ = pca_svd(self.x)
         x_reduced_eig, _ = pca_eig(self.x)
-        np.testing.assert_array_almost_equal_nulp(x_reduced_eig, x_reduced_svd, 5)
+        np.testing.assert_array_almost_equal_nulp(x_reduced_eig, x_reduced_svd, 4)
